f9381c71a7ee46d68581e52bc6608985561afd43
Ogheneovie Ralph Otutu <phexcom@phexcom.me>
2018-08-20 23:11:01 +0100

    Updated iRedMail download to 0.9.8
    Updated iRedMail download to 0.9.8

--- ServerSetup.sh
+++ ServerSetup.sh
@@ -557,9 +557,9 @@ function Install_GoPhish {
 
 function Install_IRedMail {
 	echo "Downloading iRedMail"
-	wget https://bitbucket.org/zhb/iredmail/downloads/iRedMail-0.9.6.tar.bz2
-	tar -xvf iRedMail-0.9.6.tar.bz2
-	cd iRedMail-0.9.6/
+	wget https://bitbucket.org/zhb/iredmail/downloads/iRedMail-0.9.8.tar.bz2
+	tar -xvf iRedMail-0.9.8.tar.bz2
+	cd iRedMail-0.9.8/
 	chmod +x iRedMail.sh
 	echo "Running iRedMail Installer"
 	./iRedMail.sh
